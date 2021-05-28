ca76ef033fa48218934fe51495b566d11cf96343
Andrew Klima <andrewklima7@gmail.com>
2021-05-28 14:18:11 -0500

    3rd commit pig latin capstone Andrew Klima

--- pig latin capstone/Program.cs
+++ pig latin capstone/Program.cs
@@ -6,7 +6,6 @@ namespace pig_latin_capstone
     {
         static void Main(string[] args)
         {
-            
             bool again = true;
             while(again == true)
             {
@@ -15,15 +14,15 @@ namespace pig_latin_capstone
                 string lower = input.ToLower();
                 
 
-                if (input == null) // also this wont work for trying to make sure the user actually input something
+                if (input.Length == 0) // also this wont work for trying to make sure the user actually input something
                 {
                     Console.WriteLine("You must type something");
-                    again = GoAgain();
                 }
                 else
                 {
                     char[] Voids = { '@', '%', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '$', '#', '&' };
-                    foreach (string letter in lower.Split())
+                    
+                    foreach (char c in lower.ToCharArray())
                     {
                         if (input.Trim().Contains(Voids[0]) || input.Trim().Contains(Voids[1]) || input.Trim().Contains(Voids[2]) || input.Trim().Contains(Voids[3]) || input.Trim().Contains(Voids[4]) || input.Trim().Contains(Voids[5]) || input.Trim().Contains(Voids[6]) || input.Trim().Contains(Voids[7]) || input.Trim().Contains(Voids[8]) || input.Trim().Contains(Voids[9]) || input.Trim().Contains(Voids[10]) || input.Trim().Contains(Voids[11]) || input.Trim().Contains(Voids[12]) || input.Trim().Contains(Voids[13]) || input.Trim().Contains(Voids[14]))
                         {
@@ -38,14 +37,12 @@ namespace pig_latin_capstone
                     }
                 }
                 again = GoAgain();
-            }
-            
+            }           
         }
 
         public static bool IsVowel(string userInput) 
         {
             string low = userInput.ToLower();
-
             char[] Vowels = { 'a', 'e', 'i', 'o', 'u' };
          
             foreach (char c in low.ToCharArray())
@@ -60,15 +57,15 @@ namespace pig_latin_capstone
                 {
                     Console.WriteLine("The first vowel in {0} is at index {1}", userInput, userInput.IndexOfAny(Vowels));
                     string[] word = userInput.Split(Vowels);
-                    string sub = userInput.Substring(userInput.IndexOfAny(Vowels), userInput.Length - 1);
+                    string sub = userInput.Substring(userInput.IndexOfAny(Vowels));
 
-                    Console.WriteLine(sub + word[0] + "ay");
+                    Console.WriteLine(sub.Trim() + word[0] + "ay");
 
-                    string firstLetter = userInput.Trim().Substring(0, 1);
+                    /*string firstLetter = userInput.Trim().Substring(0, 1);
                     string restOfWord = userInput.Substring(1, userInput.Length - 1); // -1 becasue .lenth will output as many charcters that there is starting at 1 and so itll alawys be one position too far since arrays start at 0 not 1. 
                                                                                         //just moving the first letter idk how to get it to move all the consonants before the first vowel
                     userInput = restOfWord.Trim() + firstLetter.Trim() + "ay";
-                    Console.WriteLine(userInput);
+                    Console.WriteLine(userInput);*/ //this is just nice notes for ranging things
                     return true;
                 }
             }
